2248c65d67ab036d4b9fa2d0842c157921362241
Thomas Stringer <git@trstringer.com>
2018-02-07 15:32:12 -0500

    fix logger reference

--- start/index.js
+++ start/index.js
@@ -30,7 +30,7 @@ module.exports = function (context, req) {
                 resource.subscriptionId,
                 resource.resourceGroupName,
                 resource.resourceName,
-                logger
+                context.log
         )))
             .then(() => {
                 context.log('Completed stopping websites');
