2d6715a536d5753504c7a5e0f31883bf943ec8da
Thomas Stringer <git@trstringer.com>
2018-02-06 19:48:39 -0500

    add logging

--- start/index.js
+++ start/index.js
@@ -1,9 +1,11 @@
-function validateParams(req) {
+function validateParams(req, logger) {
     if (!req.query.accessToken && !(req.body && req.body.accessToken)) {
+        logger('accessToken not passed');
         return false;
     }
 
     if (!req.query.resources && !(req.body && req.body.resources)) {
+        logger('resources not passed');
         return false;
     }
 }
@@ -11,7 +13,7 @@ function validateParams(req) {
 module.exports = function (context, req) {
     context.log('Beginning start of chaos event');
 
-    if (!validateParams(req)) {
+    if (!validateParams(req, context.log)) {
         context.res = {
             status: 400,
             body: "Required params are accessToken and resources"
