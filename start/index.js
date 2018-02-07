4bc6cbbc05e86da0f4ed08b938cddf69b9c901d2
Thomas Stringer <git@trstringer.com>
2018-02-06 19:42:08 -0500

    change logging and param validation

--- start/index.js
+++ start/index.js
@@ -1,17 +1,21 @@
-module.exports = function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+function validateParams(req) {
+    if (!req.query.accessToken && !(req.body && req.body.accessToken)) {
+        return false;
     }
-    else {
+
+    if (!req.query.resources && !(req.body && req.body.resources)) {
+        return false;
+    }
+}
+
+module.exports = function (context, req) {
+    context.log('Beginning start of chaos event');
+
+    if (!validateParams(req)) {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            body: "Required params are accessToken and resources"
         };
+        context.done();
     }
-    context.done();
 };
